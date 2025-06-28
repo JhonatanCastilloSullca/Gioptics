46c11da183eda8a732b24d873e65c434eb94bf33
David Miranda <dmirandatarco@gmail.com>
2025-06-28 10:58:43 -0500

    ultimo

--- resources/views/pdf/factura.blade.php
+++ resources/views/pdf/factura.blade.php
@@ -163,8 +163,8 @@
 </head>
 <body>
     <div class="header">
-        <img src="{{public_path('img/logo-gstore.png')}}" class="img-logo" alt="logo">
-        {{public_path('img/logo-gstore.png')}}
+        <img src="{{asset('img/logo-gstore.png')}}" class="img-logo" alt="logo">
+        {{asset('img/logo-gstore.png')}}
         <div class="header-details">
             <p class="header-details-text">RUC 10446103071</p>
             <p class="header-details-text">{{$venta->sucursal->direccion}}</p>
